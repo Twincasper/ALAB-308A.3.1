13972f5072fa3e5ce74f02b34c3e51a9fe8648e2
Justin Watson <61891187+Twincasper@users.noreply.github.com>
2024-10-17 15:28:35 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+
+  const userInfo = Promise.all([central[id], vault[id]]);
+  console.log(userInfo);
+}
+
+getUserData(2);
+
+// {
+//     id: number,
+//     name: string,
+//     username: string,
+//     email: string,
+//     address: {
+//       street: string,
+//       suite: string,
+//       city: string,
+//       zipcode: string,
+//       geo: {
+//         lat: string,
+//         lng: string
+//       }
+//     },
+//     phone: string,
+//     website: string,
+//     company: {
+//       name: string,
+//       catchPhrase: string,
+//       bs: string
+//     }
+// }
